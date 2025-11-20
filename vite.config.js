f4b5c1a44c40fe2d15492e2beaa0acec1b548c61
Ömer ÖZDEMİR <ozd.omer17@gmail.com>
2025-11-20 20:03:27 +0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,25 @@
-export default {
-  base: "/Cinemania-Project/",
-};
+import { defineConfig } from "vite";
+import { glob } from "glob";
+import injectHTML from "vite-plugin-html-inject";
+
+export default defineConfig(({ command }) => {
+  return {
+    // ... diğer ayarların ...
+    
+    // BURAYI EKLE: GitHub Repo adını buraya yaz
+    base: "/Cinemania-Project/", 
+
+    root: "src", 
+    build: {
+      outDir: "../dist", 
+      emptyOutDir: true,
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync("./src/*.html"),
+      },
+    },
+    plugins: [
+      injectHTML(),
+    ],
+  };
+});
